ae3c8c1b0ca19a457774f3a073a8c81ea16a9913
Davide Salomoni <davide@infn.it>
2020-12-26 12:33:35 +0100

    subprocess example

--- dbaccess/check_docker_ps.py
+++ dbaccess/check_docker_ps.py
@@ -12,9 +12,9 @@ lines = sp.stdout.split('\n')
 # if there are 3 lines, the container is running.
 # if there are less than 2 or more than 3 lines, something went wrong with the docker ps command.
 
-if len(lines) == 3:
-    print("The container %s is running." % container_name)
-elif len(lines) == 2:
+if len(lines) == 2:
     print("The container %s is not running." % container_name)
+elif len(lines) == 3:
+    print("The container %s is running." % container_name)
 else:
     print("Something is wrong with the docker ps command!")
